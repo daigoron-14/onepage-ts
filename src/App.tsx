7deae2fdc3881b4e8eea13122ae0c606707a1219
daigoron-14 <maru0628.daigoron14@gmail.com>
2022-02-15 14:12:32 +0900

    change index

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./styles.css";
 
-function App() {
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import styled from "styled-components";
+import emotionReset from "emotion-reset";
+import { Global, css } from "@emotion/react";
+
+import { DashBoard } from "../src/components/pages/dashboard";
+import { SignUp } from "../src/components/pages/auth/signup";
+import { SignIn } from "../src/components/pages/auth/signin";
+
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Global
+          styles={css`
+            ${emotionReset}
+            *, *::after, *::before {
+              box-sizing: border-box;
+              -moz-osx-font-smoothing: grayscale;
+              -webkit-font-smoothing: antialiased;
+              font-smoothing: antialiased;
+            }
+          `}
+        />
+        <Routes>
+          <Route path="/signin" element={<SignIn />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/dashboard/*" element={<DashBoard />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
 
-export default App;
+const RightSide = styled.div`
+  margin-left: 254px;
+  padding: 20px;
+`;
+
+const SideMenu = styled.div`
+  flex: 0 0 auto;
+  width: 15.625rem;
+  white-space: nowrap;
+  border: none;
+`;
